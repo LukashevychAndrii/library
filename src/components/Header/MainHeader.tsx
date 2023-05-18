cd10608d0ee704abf6d3a01c1df1f29795dae602
Andrii Lukashevych <test@email.com>
2023-05-18 12:54:56 +0300

    add header

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "./MainHeader.module.scss";
+import { Link, NavLink } from "react-router-dom";
+import { ReactComponent as BookIcon } from "../../img/SVG/book1.svg";
+
+const getStyle = ({ isActive }: { isActive: boolean }) =>
+  isActive
+    ? `${styles["header__item--active"]} ${styles["header__item"]}`
+    : styles["header__item"];
 
 const MainHeader = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles["header"]}>
+      <NavLink className={getStyle} to="/library">
+        <BookIcon />
+        <div>Books</div>
+      </NavLink>
+    </header>
+  );
 };
 
 export default MainHeader;
