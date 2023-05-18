f058d5c55a5842501620dce29e082ed7ca31f20d
Andrii Lukashevych <test@email.com>
2023-05-18 12:54:45 +0300

    add svg-images for project

--- src/pages/RootPages/RootLayout.tsx
+++ src/pages/RootPages/RootLayout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import MainHeader from "../../components/Header/MainHeader";
 
 const RootLayout = () => {
   return (
     <>
+      <MainHeader />
       <main>
         <Outlet />
       </main>
